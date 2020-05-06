b8773010ab0c785c626bb6271950f585d1d37945
jcapelladesto <j.capellades.to@gmail.com>
2020-05-06 14:20:55 +0200

    update namespace and doc datasets

---
+++ R/formulaTables-data.R
@@ -0,0 +1,13 @@
+#' Example data frames input for autoQ function
+#'
+#' @docType data
+#'
+#' @usage data(formulaTables)
+#'
+#' @format A data.frame containing minimal information to run \link{autoQ} with extdata files
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(formulaTables)
+"formulaTables"
